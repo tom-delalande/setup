ad26c2031c3326180d470c6f9689db39f10feeb2
Tom Delalande <80664757+tom-delalande@users.noreply.github.com>
2025-08-26 20:13:49 +0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,13 +3,10 @@ local wezterm = require("wezterm")
 return {
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 18.0,
-	-- font_size = 24.0,
 	enable_tab_bar = false,
-	window_decorations = "NONE",
+	window_decorations = "RESIZE",
 	color_scheme = "Catppuccin Mocha",
 	window_close_confirmation = "NeverPrompt",
-	-- window_background_opacity = 0.3,
-	-- macos_window_background_blur = 20
 	keys = {
 		{
 			key = "w",
